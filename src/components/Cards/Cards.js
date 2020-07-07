a57ad30c73e035b74facd2c5b8c818419332470d
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-07-07 12:00:29 +0530

    Cards and CountUp

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
+import './Cards.css';
 
 const Cards = (props) => {
-    console.log(props.data);
+    if(!props.data.confirmed)
+    {
+        return 'Loading...';
+    }
     return (
-        <h1>Cards</h1>
+        <div className="container">
+            <Grid container spacing={3}>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography variant="h5">
+                            <CountUp
+                                start={0}
+                                end={props.data.confirmed.value}
+                                duration={2}
+                                seperator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary">REAL DATA</Typography>
+                        <Typography variant="body2">Nmber of active cases</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">REAL DATA</Typography>
+                        <Typography variant="body2">Nmber of active cases</Typography>
+                    </CardContent>
+                </Grid>
+
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography variant="h5">REAL DATA</Typography>
+                        <Typography color="textSecondary">REAL DATA</Typography>
+                        <Typography variant="body2">Nmber of active cases</Typography>
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 
