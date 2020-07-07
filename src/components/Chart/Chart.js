14009528afb95dcfa2c1b9b783b04af65082f94e
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-07-07 17:44:27 +0530

    plotted chart

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {fetchDailyData} from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+
+import './Chart.css';
 
 const Chart= () => {
+
+    const [dailyData, setDailyData] = useState([]);
+
+    useEffect(() => {
+        const fetchedDailyData = async() => {
+            const data = await fetchDailyData();
+            setDailyData(data);
+        }
+        console.log(dailyData);
+        fetchedDailyData();
+        
+    })
+
+    const lineChart = (
+        dailyData.length ?
+        (<Line 
+        data={{
+            labels: dailyData.map(({ date }) => date),
+            datasets:[{
+                data:dailyData.map(({ confirmed }) => confirmed),
+                label: 'Infected',
+                borderColor: '#3333ff',
+                fill: true
+            },{
+                data: dailyData.map(({ deaths }) => deaths),
+                label: 'Deaths',
+                borderColor: '#3333ff',
+                backgroundColor: 'rgba(255, 0, 0, 0.5)',
+                fill: true
+            }]
+        }}
+        />) : null
+
+    );
+
     return (
-        <h1>Chart</h1>
+        <div className="container">
+            {lineChart}
+        </div>
     )
 }
 
