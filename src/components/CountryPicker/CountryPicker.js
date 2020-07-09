720837003100c3e5c99f24b3ab021480cbc39318
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-07-09 13:19:42 +0530

    CountryPicker UI

--- src/components/CountryPicker/CountryPicker.js
+++ src/components/CountryPicker/CountryPicker.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { NativeSelect, FormControl } from '@material-ui/core';
 import { fetchCountries } from '../../api';
 
+import world from '../../images/world.png';
 import './CountryPicker.css';
 
 const CountryPicker = ({ countryChangeHandler }) => {
@@ -17,9 +18,12 @@ const CountryPicker = ({ countryChangeHandler }) => {
         fetchedCountry();
     }, [setCountry])
 
-    return (<div>
+    return (
+    <div className="countryPicker">
+        
         <FormControl className="formControl" >
-            <NativeSelect defaultValue="" onChange={(e) => countryChangeHandler(e.target.value)}>
+        <img src={world} alt="symptom" className="world" style={{display:'flex',width:'30%',alignItems:'center'}} />
+            <NativeSelect style={{display:'flex',width:'70%',alignItems:'center',height:'max-content'}} defaultValue="" onChange={(e) => countryChangeHandler(e.target.value)}>
                 <option value="">Global</option>
                 {
                     country.map((countryName, i)=> <option key={i} value = {countryName}>{countryName}</option>)
--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import symptoms from '../../images/sym.png';
 
-import {CardContent, Typography, Grid } from '@material-ui/core';
+import {Card, CardContent, Typography, Grid, ButtonBase } from '@material-ui/core';
 import './HeroSection.css';
 
 const HeroSection = () => {
@@ -12,27 +11,24 @@ const HeroSection = () => {
             <Grid container spacing={3}>
                 <Grid item xs={12} md={3} className="row">
                     <CardContent>
-                            <img src={symptoms} alt="symptom" className="symp" />
+                            
                     </CardContent>
                 </Grid>
 
                 <Grid item xs={12} md={8} className="row">
                     <CardContent>
-                    <Typography color="textPrimary">Corona viruses are a large family of viruses which may cause illness
-in animals or humans. In humans, several coronaviruses are known
-to cause respiratory infections ranging from the common cold to more
-severe diseases such as Middle East Respiratory Syndrome (MERS)
-and Severe Acute Respiratory Syndrome (SARS). The most recently
-discovered coronavirus causes coronavirus disease COVID-19.
-COVID-19 is the infectious disease caused by the most recently
-discovered corona virus. This new virus and disease were unknown
-before the outbreak began in Wuhan, China, in December 2019. </Typography>
-<Typography variant="h5">Symptoms:</Typography>
-                    <Typography color="textPrimary">The most common symptoms of COVID-19 are fever, tiredness, and
-dry cough. Some patients may have aches and pains, nasal
-congestion, runny nose, sore throat or diarrhea. These symptoms are
-usually mild and begin gradually. Some people become infected but
-don’t develop any symptoms and don't feel unwell. </Typography>
+                    <Grid container spacing={3}>
+                        <Grid component={Card} item xs direction="column" spacing={3}>
+                        <Typography variant="h5" >COVID-19 is a disease caused by a new strain of coronavirus. 'CO' stands for corona, 'VI' for virus, and 'D' for disease. Formerly, this disease was referred to as '2019 novel coronavirus' or '2019-nCoV.'</Typography>
+                        </Grid>
+                        <Grid component={Card} item xs direction="column" spacing={3}>
+                            <Typography color="textSecondary" gutterBottom className="box-heading">Infected</Typography>
+                            <Typography variant="h4">
+                                
+                                </Typography>
+                            <Typography color="textSecondary"></Typography>
+                        </Grid>
+                    </Grid>
                     </CardContent>
                 </Grid>
               </Grid>
