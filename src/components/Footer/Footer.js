ba14bae805b0aa59540e4a071f09acca8bd31c89
Ramneek Singh <48586823+ramneek008@users.noreply.github.com>
2020-07-11 12:02:34 +0530

    added Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => {
+
+    return(
+        <div className="footer">
+            <h3>Made with 💙 By : Ramneek Singh</h3>
+        </div>
+    )
+
+}
+
+export default Footer;
